1cf2071a9e117d215dcb6ab7effbd698c3c60d84
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-08-11 10:57:38 -0700

    Update React Cloudinary.jsx

--- Cloudinary/React Cloudinary.jsx
+++ Cloudinary/React Cloudinary.jsx
@@ -1,5 +1,6 @@
 
-// Cloudinary File Upload Fetcher Function
+// ----------------Cloudinary File Upload Fetcher Function------------------------------//
+
 export const cloudinaryFetcher = async( image, setUrl) => {
 
     const data = new FormData();          
@@ -22,6 +23,48 @@ export const cloudinaryFetcher = async( image, setUrl) => {
         .catch((err) => console.log(err))    
   }
 
-//   Invoke Fetcher Function
 
-cloudinaryFetcher (image, set)
+
+// -----------------------Another component-------------------------------//
+
+import { useState } from "react";
+
+const Tech = () => {
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    // Cloudinary fetcher function
+    await cloudinaryFetcher(image, setUrl);
+
+    // get url from cloudinary
+    console.log(url);
+  }
+
+  return (
+    <>
+      <div className="flex flex-col gap-8 items-center w-full overflow-y-visible">
+        <div className="flex flex-col gap-2 items-center text-gray-200">
+          <h1 className="text-3xl font-bold">Add New Tech Stack</h1>
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className=" flex flex-col gap-7 text-sm text-white"
+        >
+          <input
+            type="file"
+            onChange={(e) => setImage(e.target.files[0])}
+            className="p-3 rounded-md w-[350px] md:w-[550px] bg-gray-700/30"
+          />
+
+          <button className="btn primary_bg w-fit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Tech;
