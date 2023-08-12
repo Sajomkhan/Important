6c63c1515ed42ff902c16aa950d5f6c05da6e6f9
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-08-12 10:06:03 -0700

    Update multer.js

--- Multer/Multer for cloudinary/multer.js
+++ Multer/Multer for cloudinary/multer.js
@@ -1,9 +1,10 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  filename: function (req,file,cb) {
-    cb(null, file.originalname)
-  }
+  filename: (req, file, callback) => {
+    const fileName = Date.now() + '-' + file.originalname
+    callback(null, fileName)
+}
 });
 
 const upload = multer({storage: storage});
