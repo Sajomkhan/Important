a394771a48c92412d61140db8623f802d475b084
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-05 23:39:00 +0600

    Create tailwind.jsx

---
+++ CSS/tailwind.jsx
@@ -0,0 +1,18 @@
+
+// Grid System
+
+        <form className="grid grid-cols-6 gap-4 mb-5">
+          <input
+            className="col-span-3 p-2 border"
+            type="text"
+            placeholder="Enter Item"
+          />
+          <input
+            className="col-span-2 p-2 border"
+            type="text"
+            placeholder="Enter $"
+          />
+          <button className="bg-gray-900 hover:bg-gray-950 p-2" type="submit">
+            +
+          </button>
+        </form>
