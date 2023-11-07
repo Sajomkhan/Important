b361d1ff1b910f671a3736d72202637fbfd50e37
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-08 00:59:02 +0600

    Update Impotent.jsx

--- React.js/Impotent.jsx
+++ React.js/Impotent.jsx
@@ -8,8 +8,19 @@
 const totalPrice = items.reduce((sum, item) => sum + parseFloat(item.price), 0);
 
 
+
 // ---------------Push array into another array-----------------------//
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 array1.push(...array2);
 console.log(array1); // [1, 2, 3, 4, 5, 6]
+
+
+
+  // ----------------Define a function to handle the key press event----------------//
+  function handleKeyPress(e) {
+    if (e.keyCode === 13) {
+      myFunction();  // call back function
+    }
+  }
+  document.addEventListener("keydown", handleKeyPress);
