7e5b3c0357803a5a039694b6affbecbbbab9053c
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-07 23:44:42 +0600

    Create Impotent.jsx

---
+++ React.js/Impotent.jsx
@@ -0,0 +1,16 @@
+
+// ------------------Calculate the sum of the price values-----------------//
+ const [items, setItems] = useState([
+    { name: "Coffee", price: "6.55" },
+    { name: "Movie", price: "20.00" },
+    { name: "Candy", price: "4.55" },
+  ]);
+
+const totalPrice = items.reduce((sum, item) => sum + parseFloat(item.price), 0);
+
+
+// ---------------Push array into another array-----------------------//
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+array1.push(...array2);
+console.log(array1); // [1, 2, 3, 4, 5, 6]
