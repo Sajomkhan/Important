aabb81f882818a6ca3469f7b05768ecbb7021a89
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-17 00:41:55 +0600

    Update connection.js

--- DB/MongoDB/connection.js
+++ DB/MongoDB/connection.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL)
@@ -21,14 +20,15 @@ module.exports = connectDB
 
 import mongoose from "mongoose";
 
-// if one time connected no need ferther connecting
+const connection = {};
+
 export const connectDB = async () => {
-  const connection = {};
   try {
     if (connection.isConnected) return;
     const db = await mongoose.connect(process.env.DB_URL);
-    connection.isConnected = db.connection[0].readyState;
+    connection.isConnected = db.connections[0].readyState;
   } catch (error) {
+    console.log(error)
     throw new Error(error);
   }
 };
