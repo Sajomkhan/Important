8d8898779e87e0a46fd84343d772180febd7e1e6
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-19 01:11:10 +0600

    Update search.jsx

--- Next.js/search.jsx
+++ Next.js/search.jsx
@@ -1,24 +1,28 @@
 //--------------------searct.jsx--------------------//
 
 "use client";
+// have to install--------- npm i use-debounce@9.0.4
+// and use as like that -------useDebouncedCallback((e)=>{},300)
+
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdSearch } from "react-icons/md";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (e) => {
+  const handleSearch = useDebouncedCallback((e) => {
     const params = new URLSearchParams(searchParams);
+
     if (e.target.value) {
-        e.target.value > 1
-            && params.set("q", e.target.value);
+      e.target.value.length > 1 && params.set("q", e.target.value);
     } else {
       params.delete("q");
     }
     replace(`${pathname}?${params}`);
-  };
+  }, 400);
 
   return (
     <div className="flex gap-1 items-center w-60 bg-[var(--bgHover)] rounded-lg px-3 py-2">
@@ -32,7 +36,9 @@ const Search = ({ placeholder }) => {
     </div>
   );
 };
+
 export default Search;
+
 
 //--------------------fetcher.js--------------------//
 
