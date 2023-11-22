e563629e3ef48eb0343b24181ef31bce155740aa
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-23 00:10:20 +0600

    Update importent.jsx

--- Next.js/importent.jsx
+++ Next.js/importent.jsx
@@ -19,3 +19,33 @@ const page = () => {
 };
 
 export default page;
+
+
+//----------------action.js-----------------------------//
+import { connectDB } from "./connection";
+import { User } from "./models";
+
+export const addUser = async (formData) => {
+  "use server";
+  const { username, email, password, img, isAdmin, isActive, address } =
+    Object.fromEntries(formData);
+
+  try {
+    connectDB;
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img,
+      isAdmin,
+      isActive,
+      address,
+    });
+
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to create user");
+  }
+};
+
