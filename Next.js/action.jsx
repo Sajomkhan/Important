7c62310362640abfe3f1689e01458e61c859cd09
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-23 23:18:29 +0600

    Update action.jsx

--- Next.js/action.jsx
+++ Next.js/action.jsx
@@ -22,27 +22,25 @@ export default page;
 
 
 //----------------action.js-----------------------------//
-import { connectDB } from "./connection";
-import { User } from "./models";
-
 export const addUser = async (formData) => {
   "use server";
   const { username, email, password, img, isAdmin, isActive, address } =
     Object.fromEntries(formData);
-    // or  const data = Object.fromEntries(formData);
 
   try {
     connectDB;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img,
       isAdmin,
       isActive,
       address,
     });
-    // or  const newUser = new User(data)
 
     await newUser.save();
   } catch (err) {
@@ -52,3 +50,16 @@ export const addUser = async (formData) => {
   revalidatePath("/dashboard/users");
   redirect("/dashboard/users");
 };
+
+// export const addUser = async (formData) => {
+//   const data = Object.fromEntries(formData);
+
+//   try {
+//     connectDB;
+//     const newUser = await User.create(data);
+//     return new NextResponse(JSON.stringify(newUser), { status: 200 });
+//   } catch (err) {
+//     console.log(err);
+//     throw new Error("Failed to create user");
+//   }
+// };
