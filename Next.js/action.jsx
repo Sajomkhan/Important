0bba5fe0becc7ab9bb414cbf4f7e2ff252fc7aa7
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-23 22:14:57 +0600

    Create action.jsx

---
+++ Next.js/action.jsx
@@ -0,0 +1,50 @@
+
+// direct get data form "formData" and post action without server
+const page = () => {
+  
+  const handleFrom = async (formData) => {
+    "use server";
+    const username = formData.get("username");
+    console.log("Hello", username);
+  };
+
+  return (
+    <div>
+      <form action={handleFrom}>
+        <input type="text" name="username" />
+        <button>Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
+
+
+//----------------action.js-----------------------------//
+import { connectDB } from "./connection";
+import { User } from "./models";
+
+export const addUser = async (formData) => {
+  "use server";
+  const { username, email, password, img, isAdmin, isActive, address } =
+    Object.fromEntries(formData);
+
+  try {
+    connectDB;
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img,
+      isAdmin,
+      isActive,
+      address,
+    });
+
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to create user");
+  }
+};
