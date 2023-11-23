0f62b94bb6b306e9282a3080dc8d1381879cf075
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-23 23:21:31 +0600

    Update importent.jsx

--- Next.js/importent.jsx
+++ Next.js/importent.jsx
@@ -30,20 +30,21 @@ export const addUser = async (formData) => {
   const { username, email, password, img, isAdmin, isActive, address } =
     Object.fromEntries(formData);
     // or  const data = Object.fromEntries(formData);
-
   try {
     connectDB;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img,
       isAdmin,
       isActive,
       address,
     });
     // or  const newUser = new User(data)
-
     await newUser.save();
   } catch (err) {
     console.log(err);
@@ -52,3 +53,16 @@ export const addUser = async (formData) => {
   revalidatePath("/dashboard/users");
   redirect("/dashboard/users");
 };
+
+// export const addUser = async (formData) => {
+//   const data = Object.fromEntries(formData);
+
+//   try {
+//     connectDB;
+//     const newUser = await User.create(data);
+//     return new NextResponse(JSON.stringify(newUser), { status: 200 });
+//   } catch (err) {
+//     console.log(err);
+//     throw new Error("Failed to create user");
+//   }
+// };
