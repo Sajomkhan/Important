9de94c9b5059a512fd0ea6d7a175649479ae0d08
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-11-29 00:28:15 +0600

    Create api-less-auth.js

---
+++ Auth/api-less-auth.js
@@ -0,0 +1,95 @@
+
+//-------------------app/authConfig.js------------------//
+export const authConfig = {
+  providers:[],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request }) {
+      const isLoggedIn = auth?.user;
+      const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", request.nextUrl));
+      }
+      return true;
+    },
+  },
+};
+
+
+//-------------------root-file/ middleware.js------------------//
+import NextAuth from "next-auth";
+import { authConfig } from "./app/authconfig";
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ['/((?!api|static|.*\\..*|_next).*)'],
+};
+
+
+//-------------------app/ auth.js------------------//
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { authConfig } from "./authconfig";
+import { connectToDB } from "./lib/utils";
+import { User } from "./lib/models";
+import bcrypt from "bcrypt";
+
+const login = async (credentials) => {
+  try {
+    connectToDB();
+    const user = await User.findOne({ username: credentials.username });
+
+    if (!user || !user.isAdmin) throw new Error("Wrong credentials!");
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!");
+
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to login!");
+  }
+};
+
+export const { signIn, signOut, auth } = NextAuth({
+  ...authConfig,
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials);
+          return user;
+        } catch (err) {
+          return null;
+        }
+      },
+    }),
+  ],
+  // ADD ADDITIONAL INFORMATION TO SESSION
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.username = user.username;
+        token.img = user.img;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.username = token.username;
+        session.user.img = token.img;
+      }
+      return session;
+    },
+  },
+});
