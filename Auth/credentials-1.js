d5bdcace5cc49c826d68871b4b437245253d7fa9
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-12-13 01:36:16 +0600

    Update credentials-1.js

--- Auth/credentials-1.js
+++ Auth/credentials-1.js
@@ -2,134 +2,85 @@
 
 //------------------------ package.json -------------------------//
   "dependencies": {
-    "@auth/mongodb-adapter": "^2.0.3",
-    "@aws-sdk/client-s3": "^3.438.0",
     "bcrypt": "^5.1.1",
-    "mongodb": "^6.2.0",
     "mongoose": "^7.6.3",
     "next": "14.0.0",
     "next-auth": "^4.24.4",
-    "react": "^18",
-    "react-dom": "^18",
   },
 
 
 
-//------------------------ libs/connectDB -------------------------//
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient } from "mongodb"
+//------------------------ utils/connect.js -------------------------//
+import mongoose from "mongoose";
 
-if (!process.env.DB_URL) {
-  throw new Error('Invalid/Missing environment variable: "DB_URL"')
-}
-const uri = process.env.DB_URL
-const options = {}
-
-let client
-let clientPromise;
-
-if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
+export async function connectDB(){
+    try {
+        await mongoose.connect(process.env.DB_URI)
+        console.log("DB is connected");
+    } catch (error) {
+        console.log("Error while connecting.", error);        
+    }
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise
-
-
-
-//------------------------app/api/auth/[...nextauth]/route.ts-------------------------//
-import clientPromise from "@/libs/mongoConnect";
-import {UserInfo} from "@/models/UserInfo";
-import bcrypt from "bcrypt";
-import * as mongoose from "mongoose";
-import {User} from '@/models/User';
-import NextAuth, {getServerSession} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
+//------------------------ api/auth/[...nextauth]/route.jsx -------------------------//
+import NextAuth from "next-auth/next";
+import { CredentialsProvider } from "next-auth/providers/credentials";
 
 export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/login",
+  },
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
     CredentialsProvider({
-      name: 'Credentials',
-      id: 'credentials',
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-
-        mongoose.connect(process.env.DB_URL);
-        const user = await User.findOne({email});
-        const passwordOk = user && bcrypt.compareSync(password, user.password);
-        
-        if (passwordOk) {
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials);
+          console.log("user:", user);
           return user;
+        } catch (error) {
+          console.log("Error = ", error);
+          return null;
         }
-        return null
-      }
-    })
+      },
+    }),
   ],
 };
 
-export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({email:userEmail});
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
-}
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }
+
+export { handler as GET, handler as POST };
 
 
 
-//------------------------app/api/register/route.ts-------------------------//
-import {User} from "@/models/User";
+
+//------------------------api/register/route.jsx-------------------------//
+import { connectDB } from "../../utils/connect"
+import User from "../../models/userModel"
+import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import mongoose from "mongoose";
 
-export async function POST(req) {
-  const body = await req.json();
-  mongoose.connect(process.env.MONGO_URL);
-  const pass = body.password;
-  if (!pass?.length || pass.length < 5) {
-    new Error('password must be at least 5 characters');
-  }
-  const notHashedPassword = pass;
-  const salt = bcrypt.genSaltSync(10);
-  body.password = bcrypt.hashSync(notHashedPassword, salt);
-
-  const createdUser = await User.create(body);
-  return Response.json(createdUser);
+export const POST = async (req) => {
+try {
+    await connectDB()
+    const {email, password} = await req.json();
+    const exists = await User.findOne({email})
+    // const exists = await User.findOne({$or:[{email},{username}]})
+    if(exists){
+        return NextResponse.json({message: "Email already exist"}, {status: 500})
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await User.create({email, password: hashedPassword})
+    return NextResponse.json({message: "User registered"}, {status: 201});
+} catch (error) {
+    console.log("Error while registering user.", error);
+    return NextResponse.json({message: "Error occured while registering the user"}, {status: 500});
+}
 }
 
 
-//------------------------app/api/register/route.ts-------------------------//
+//------------------------app/api/register/page.jsx-------------------------//
 "use client";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
@@ -170,4 +121,144 @@ export default function RegisterPage() {
       </form>
   )}
 
+
+//------------------------app/api/signin/page.jsx-------------------------//
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [showPasswordText, setShowPasswordText] = useState("password");
+  const [info, setInfo] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+  const [pending, setPending] = useState(false);
+
+  const handleInput = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!info.email || !info.password) {
+      setError("Must provide all the credentials.");
+    }
+    try {
+      setPending(true);
+      const res = await signIn("credentials", {
+        email: info.email,
+        password: info.password,
+        redirect: false,
+      });
+      if (res.error) {
+        setError("invalid Credentials");
+        setPending(false);
+        return;
+      }
+      router.replace("/");
+    } catch (error) {
+      console.log("Something went wrong", error.message);
+      setPending(false);
+    }
+  };
+
+  return (
+    <div className="mx-auto max-w-screen-xl bg-gray-50  px-4 py-16 sm:px-6 lg:px-8 shadow-lg">
+      <div className="mx-auto max-w-lg text-center">
+        <h1 className="text-2xl font-bold sm:text-3xl">Sign in your account</h1>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto mb-0 mt-8 max-w-md space-y-4"
+      >
+        <div>
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+
+          <div className="relative">
+            <input
+              type="email"
+              name="email"
+              onChange={(e) => handleInput(e)}
+              className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+              placeholder="Enter email"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+
+          <div className="relative">
+            <input
+              type={showPasswordText}
+              name="password"
+              onChange={(e) => handleInput(e)}
+              className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+              placeholder="Enter password"
+            />
+
+            <span
+              onClick={() => {
+                showPasswordText === "password"
+                  ? setShowPasswordText("text")
+                  : setShowPasswordText("password");
+              }}
+              className="absolute inset-y-0 end-0 grid place-content-center px-4 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  className={`${
+                    showPasswordText === "text" ? "text-yellow-600 " : ""
+                  }`}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        {error && (
+          <p className="font-light text-sm text-red-600 ml-4">{error}</p>
+        )}
+
+        <div className="flex items-center justify-between">
+          <p className="text-sm text-gray-500">
+            No account?{" "}
+            <a className="underline" href="/register">
+              Sign up
+            </a>
+          </p>
+
+          <button
+            type="submit"
+            className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+            disabled={pending ? true : false}
+          >
+            {pending ? "Wait for Sign in" : "Sing in"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
 
