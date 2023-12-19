a7faf335e87058af576bafc58a56d747a4e4a24a
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-12-19 11:41:06 +0600

    Update credentials-auth.jsx

--- Auth/credentials-auth.jsx
+++ Auth/credentials-auth.jsx
@@ -164,7 +164,7 @@ export default function Navbar() {
   return (
       <navbar>
           
-         { name? (
+         { name ? (
              <p>{name}</p>
           ):(
              <a href="/login">Sign in</a>
