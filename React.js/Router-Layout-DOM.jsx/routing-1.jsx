fee21fde12b72b401a0b33a6242cffa861f40a2f
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-12-23 22:19:47 +0600

    updated

---
+++ React.js/Router-Layout-DOM.jsx/routing-1.jsx
@@ -0,0 +1,44 @@
+
+
+//-------------------------------src/App-----------------------------//
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import FoodPage from "./pages/FoodPage";
+import Carts from "./features/carts/Carts";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Footer from "./components/Footer";
+
+function App() {
+  const Layout = () => {
+    return (
+      <>
+        <div className="fixed z-40 top-0 w-full">
+          <Navbar />
+        </div>
+        <div className="md:w-9/12 mx-auto mt-20">
+          <Outlet />
+        </div>
+        <Footer />
+      </>
+    );
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/foodpage" element={<FoodPage />} />
+        </Route>
+        <Route path="/carts" element={<Carts />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
