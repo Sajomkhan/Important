dcedfc475f41dd009b0f281b96e74e3627864134
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-03 23:21:52 +0600

    Update clerk-auth.ts

--- Auth/clerk-auth.ts
+++ Auth/clerk-auth.ts
@@ -39,10 +39,7 @@ export default function RootLayout({
 //-------------------- root file/middleware.ts ---------------------//
     
 import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
 export default authMiddleware({
     publicRoutes: ['/']    // add routes address to be public as needed.
 });
