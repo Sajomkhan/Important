561a2dfea19592e14bbaf57ec1212267fa6173e8
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-04 00:44:59 +0600

    Update clerk-auth-2.ts

--- Auth/clerk-auth-2.ts
+++ Auth/clerk-auth-2.ts
@@ -1,16 +1,16 @@
+// These code is taken from events project
+// --------------------------------------------------------------------
 // clerk.com  -->  dashboard --> add application --> Next.js[select] --> [coppe the secret key & past in .env.local]
 // --> continue in docs and flow the instruction
 // for custome Sign in and see the docs --> Create custom sign-up and sign-in [at the end]
 
 
 npm install @clerk/nextjs
-
   
 //-------------------- .env ---------------------//
 
 NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_YW1hemVkLWdvYmxpbi0yNy5jbGVyay5hY2NvdW50cy5kZXYk
 CLERK_SECRET_KEY=sk_test_w4vsvNuMU4Q6D1jhD6C7Z0RFsnfKkKZsxCfuIZFkiu
-
 
 
 //-------------------- /app/layout.tsx ---------------------//
@@ -36,9 +36,7 @@ export default function RootLayout({
 }
 
 
-
 //-------------------- root file/middleware.ts ---------------------//
-
 
 import { authMiddleware } from "@clerk/nextjs";
  
@@ -77,6 +75,26 @@ import { SignUp } from "@clerk/nextjs";
 export default function page(){
     return <SignUp/>
 }
+
+//-------------------- .env.local ---------------------//
+// for custom sign-in and sign-up add in  .env.local 
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL=/
+NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL=/
+
+//--------------------  app/(auth)/layout.tsx ---------------------// for centerize login page
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
+
+
 
 //-------------------- (components)/Navbar.tsx ---------------------//
 
