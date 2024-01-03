213cccd52032dbb2a2a64b3a304b9f4675e6614b
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-03 23:30:10 +0600

    Update clerk-auth.ts

--- Auth/clerk-auth.ts
+++ Auth/clerk-auth.ts
@@ -49,6 +49,30 @@ export const config = {
 };
 
 
+//------(or)-------------- root file/middleware.ts  ---------------------//
+
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+    publicRoutes: [    // add routes address to be public as needed.
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+    ],
+    ignoredRoutes: [
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+    ]
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+
+
 //-------------------- (components)/Navbar.tsx ---------------------//
 
 "use client"
