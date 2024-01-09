e19a419aa37e0932bde5c6678663b8919104ffce
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-01-10 01:06:02 +0600

    updated

--- Upload File/upload.next.js.jsx
+++ Upload File/upload.next.js.jsx
@@ -7,7 +7,7 @@
 import{ useState } from "react";
 
 const Home = () => {
-    
+
   const [file, setFile] = useState<File>();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -18,10 +18,11 @@ const Home = () => {
       const data = new FormData();
       data.set("file", file);
 
-      const res = fetch("api/upload", {
+      const response = fetch("api/upload", {
         method: "POST",
         body: data,
       });
+      return response
 
     } catch (error: any) {
       console.log(error);
@@ -45,3 +46,20 @@ export default Home;
 
 
 //=================app/api/upload/route.ts=================//
+
+export async function POST(request: NextRequest) {
+    const data = await request.formData()
+    const file: File | null =  data.get('file') as unknown as File
+
+    if(!file) {
+        return NextResponse.json({ success: false })
+    }
+
+    const bytes = await file.arrayBuffer()
+    const buffer = Buffer.from(bytes)
+
+    console.log(buffer);
+
+    return NextResponse.json({success: true, message: 'The file was sent'})
+    
+}
