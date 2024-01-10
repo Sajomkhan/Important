dbaaddca7269be94a9a87b6123205da47c1cc98c
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-11 00:09:58 +0600

    Update Metadata.jsx

--- Next.js/Metadata.jsx
+++ Next.js/Metadata.jsx
@@ -20,3 +20,62 @@ export default function PostId({ params}) {
         Post: {params.id}
     </main>
 }
+
+//===================Dynamic Metadata=====================//
+//===================== app/layout.tsx====================//
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Home Page",
+    template: "%s | Test App",
+  },
+  description: "My Testing App Description",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+      <html lang="en">
+        <body >
+            {children}
+        </body>
+      </html>
+  );
+}
+
+
+//===================== app/products/page.tsx====================//
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Products Page",
+    description: "Products Description",
+  };
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+export default Products
+
+
+//===================== app/about/page.tsx====================//
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About Page",
+    description: "About Description",
+  };
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+export default About
