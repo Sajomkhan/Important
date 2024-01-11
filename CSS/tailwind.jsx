5fd06089bf4d2537e226a24bf56aef329420645e
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-12 00:43:55 +0600

    Update tailwind.jsx

--- CSS/tailwind.jsx
+++ CSS/tailwind.jsx
@@ -18,7 +18,7 @@
         </form>
 
 // -----------------image background -------------- //
-        <div className='h-screen w-screen bg-cover bg-center bg-[url("/image/hero.png")]'>
+        <div className='h-screen w-screen bg-cover bg-center bg-[url("/image/hero.png")]'>        // image location:  public/image/hero
           <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 ">
             {children}
           </div>
