7acf0758a6144fabb9de4e5957df2c492b254718
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-01-12 23:06:04 +0600

    Update tailwind.jsx

--- CSS/tailwind.jsx
+++ CSS/tailwind.jsx
@@ -18,8 +18,22 @@
         </form>
 
 // -----------------image background -------------- //
+                
         <div className='h-screen w-screen bg-cover bg-center bg-[url("/image/hero.png")]'>        // image location:  public/image/hero
           <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 ">
             {children}
           </div>
         </div>
+
+// -----------------image background with blund over extra layer -------------- //
+                
+    <div className="relative">
+      <Image
+        className="w-full h-[540px] absolute -z-20 object-cover object-center"
+        src="/image/hero.png"
+        width={1200}
+        height={800}
+        alt="Hero Image"
+      />
+      <div className="w-full h-[540px] absolute -z-10 bg-teal-100/70" />
+    </div>
