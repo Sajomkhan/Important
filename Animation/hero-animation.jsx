3471337b6b13a5e2ccc0d931c2beb25d13f8c9f5
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-03-02 21:59:28 +0600

    Create hero-animation.jsx

---
+++ Animation/hero-animation.jsx
@@ -0,0 +1,23 @@
+
+<body>
+    <div class="hero">
+      <nav>
+        <img class="logo" src="images/logo.png" alt="Logo" />
+        <ul>
+          <li><a href="#">Fetcher</a></li>
+          <li><a href="#">How it works</a></li>
+          <li><a href="#">Privacy</a></li>
+        </ul>
+        <div>
+          <a href="#" class="login-btn">Login</a>
+          <a href="#" class="btn">Download App</a>
+        </div>
+      </nav>
+      <div class="content">
+        <h1 class="anim">Make<br /></h1>
+        <p class="anim">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio accusamus nemo eum. Vel ex, consequuntur quae quia accusamus voluptatem harum!</p>
+        <a href="#" class="btn anim">Join Now</a>
+      </div>
+    </div>
+    <img src="images/pic.png" alt="model image" class="fetcher-image anim">
+  </body>
