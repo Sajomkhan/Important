07e3dee5d05ab6a5b54a33176adfbf2814353156
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-03-14 00:15:33 +0600

    Create importent-css.jsx

---
+++ CSS/importent-css.jsx
@@ -0,0 +1,11 @@
+
+
+// ====================== normal layout =====================//
+
+    <div className="max-w-7xl mx-auto px-4 space-y-8">
+      <Navbar />
+      <div className=" flex flex-col gap-14">
+        <UploadFrom />
+        <ImageGallery />
+      </div>
+    </div>
