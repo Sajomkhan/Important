1cb01f1a2aef0958f65663e06881fa84d5288885
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-03-14 00:13:19 +0600

    Update main-page-layout.jsx

--- Layout Page/main-page-layout.jsx
+++ Layout Page/main-page-layout.jsx
@@ -23,3 +23,12 @@
       <UploadFrom />
       <ImageGallery />
     </div>
+
+// ====================== src/page/home.tsx =====================//
+    <div className="max-w-7xl mx-auto px-4 space-y-8">
+      <Navbar />
+      <div className=" flex flex-col gap-14">
+        <UploadFrom />
+        <ImageGallery />
+      </div>
+    </div>
