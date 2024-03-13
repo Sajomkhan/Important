40c041b0d2b967446ec100d41a8ac633227e9337
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2024-03-14 00:17:43 +0600

    Update Auth-google-youtube.js

--- Firebase/Auth-google-youtube.js
+++ Firebase/Auth-google-youtube.js
@@ -13,10 +13,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth =getAuth();
+export const auth =getAuth(app);
 export const provider = new GoogleAuthProvider()
 
-export default app;
 
 // ================= Signin.js =================//
 
