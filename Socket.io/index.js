b2e28ec84a9458c21d403a576d521a9b3a43e80c
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-04-06 23:26:16 +0600

    socket updated

---
+++ Socket.io/index.js
@@ -0,0 +1,9 @@
+// SOCKET FOR SERVER
+// Send event to client use:    io
+// to send every client use:    io.emit
+// to send one client use:      io.to(socketId).emit
+// Take event from client use:  socket.on
+
+// SOCKET FOR CLIENT
+// Send event to server use:    socket.emit
+// Take event from server use:  socket.on
